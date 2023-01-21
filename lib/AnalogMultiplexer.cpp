6bfab1197f9d0c36e39576175b5673e015452b56
Jonathan Martins <web@jonathanmartins.com.br>
2023-01-20 21:46:31 -0300

    using analog mux

---
+++ lib/AnalogMultiplexer.cpp
@@ -0,0 +1,57 @@
+#include <Arduino.h>
+#include <iostream>
+
+using namespace std;
+
+#define S0 4
+#define S1 0
+#define S2 2
+#define S3 15
+
+class AnalogMultiplexer {
+public:
+  int pin;
+
+  const int PINS[4] = {S0, S1, S2, S3};
+
+  const int CHANNELS[16][4] = {
+      {LOW, LOW, LOW, LOW},    {HIGH, LOW, LOW, LOW},
+      {LOW, HIGH, LOW, LOW},   {HIGH, HIGH, LOW, LOW},
+      {LOW, LOW, HIGH, LOW},   {HIGH, LOW, HIGH, LOW},
+      {LOW, HIGH, HIGH, LOW},  {HIGH, HIGH, HIGH, LOW},
+      {LOW, LOW, LOW, HIGH},   {HIGH, LOW, LOW, HIGH},
+      {LOW, HIGH, LOW, HIGH},  {HIGH, HIGH, LOW, HIGH},
+      {LOW, LOW, HIGH, HIGH},  {HIGH, LOW, HIGH, HIGH},
+      {LOW, HIGH, HIGH, HIGH}, {HIGH, HIGH, HIGH, HIGH},
+  };
+
+  AnalogMultiplexer(int p) { pin = p; }
+
+  void setup() {
+    pinMode(pin, INPUT);
+
+    pinMode(S0, OUTPUT);
+    pinMode(S1, OUTPUT);
+    pinMode(S2, OUTPUT);
+    pinMode(S3, OUTPUT);
+
+    setChannel(0);
+  }
+
+  void setChannel(int channel) {
+    for (int i = 0; i < 4; i++) {
+      digitalWrite(PINS[i], CHANNELS[channel][i]);
+    }
+  }
+
+  float read(int channel) {
+    setChannel(channel);
+    return analogRead(pin);
+  }
+
+  float readMilliVolts(int channel) {
+    setChannel(channel);
+
+    return analogReadMilliVolts(pin);
+  }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,98 +1,177 @@
+#include "../lib/AnalogMultiplexer.cpp"
 #include "calibration.cpp"
 #include <Arduino.h>
 
 int samples = 4096;
 
-#define ThresholdInPpm 1200
+#define SignalPin 36
 
-#define MQ2pin 36
-#define MQ3pin 39
-#define MQ4pin 34
-#define MQ5pin 35
-#define MQ6pin 32
-#define MQ7pin 33
-#define MQ8pin 25
-#define MQ9pin 26
+AnalogMultiplexer mux(SignalPin);
 
 void setup() {
+
+  mux.setup();
+
   Serial.begin(115200);
   while (!Serial)
     ;
 
-  pinMode(2, OUTPUT);
-  digitalWrite(2, LOW);
-
-  pinMode(MQ2pin, INPUT);
-  pinMode(MQ3pin, INPUT);
-  pinMode(MQ4pin, INPUT);
-  pinMode(MQ5pin, INPUT);
-  pinMode(MQ6pin, INPUT);
-  pinMode(MQ7pin, INPUT);
-  pinMode(MQ8pin, INPUT);
-  pinMode(MQ9pin, INPUT);
+  delay(500);
 }
 
-float toFloat(int value) { return static_cast<float>(value); }
+// float toFloat(int value) { return static_cast<float>(value); }
 
-float ajustSignal(float value, float fromVolts, float toVolts,
-                  int resistor1InOhms, int resistor2InOhms) {
+// float ajustSignal(float value, float fromVolts, float toVolts,
+//                   int resistor1InOhms, int resistor2InOhms) {
 
-  float voltageRatio = toVolts / fromVolts;
+//   float voltageRatio = toVolts / fromVolts;
 
-  float resistor1 = toFloat(resistor1InOhms);
-  float resistor2 = toFloat(resistor2InOhms);
+//   float resistor1 = toFloat(resistor1InOhms);
+//   float resistor2 = toFloat(resistor2InOhms);
 
-  float resistorRatio = resistor2 / (resistor1 + resistor2);
+//   float resistorRatio = resistor2 / (resistor1 + resistor2);
 
-  return (value * voltageRatio) / resistorRatio;
-}
+//   return (value * voltageRatio) / resistorRatio;
+// }
 
-float getSignal(int pin) {
-  float analog = analogRead(pin);
+// float analogReadChannel(int pin, int channel) {
 
-  int rounted = round(analog);
-  float calibrated = (int)ADC_LUT[rounted];
+//   // todo, set channel
 
-  return ajustSignal(calibrated, 5.00, 3.30, 270, 510);
-}
+//   return analogRead(pin);
+// }
 
-int calculatePpm(float signal, int min, int max) {
-  float minPpm = toFloat(min);
-  float maxPpm = toFloat(max);
-  return round((signal * ((maxPpm - minPpm) / toFloat(samples))) +
-               (signal > 0 ? minPpm : 0));
-}
+// float getSignal(int pin, int channel) {
+//   float analog = analogReadChannel(pin, channel);
 
-float toPercent(float signal) { return (signal * 100.00) / toFloat(samples); }
+//   Serial.print("- analog: ");
+//   Serial.print(analog);
 
-void getInfo(int pin, int minSensorPpm, int maxSensorPpm) {
+//   int rounded = round(analog);
+//   float calibrated = (int)ADC_LUT[rounded];
+//   float ajusted = ajustSignal(calibrated, 5.00, 3.30, 270, 510);
 
-  float signal = getSignal(pin);
+//   return ajusted;
+// }
 
-  int ppm = calculatePpm(signal, minSensorPpm, maxSensorPpm);
-  if (ppm > ThresholdInPpm) {
-    digitalWrite(2, HIGH);
-  } else {
-    digitalWrite(2, LOW);
-  }
+// int calculatePpm(float signal, int min, int max) {
+//   float minPpm = toFloat(min);
+//   float maxPpm = toFloat(max);
+//   return round((signal * ((maxPpm - minPpm) / toFloat(samples))) +
+//                (signal > 0 ? minPpm : 0));
+// }
 
-  int milliVolts = analogReadMilliVolts(pin);
+// float toPercent(float signal) { return (signal * 100.00) / toFloat(samples);
+// }
 
-  // unsigned long percent = map(signal, 0, 4095, 0, 100);
-  Serial.print("- milli-volts: ");
-  Serial.print(milliVolts);
-  Serial.print(", signal: ");
-  Serial.print(signal);
-  Serial.print(", ppm: ");
-  Serial.print(ppm);
-  Serial.print(", percent: ");
-  Serial.print(toPercent(signal));
-}
+// int globalPpm = 1;
+
+// void getInfo(int pin, int minSensorPpm, int maxSensorPpm) {
+
+//   float signal = getSignal(pin, 0);
+
+//   int ppm = calculatePpm(signal, minSensorPpm, maxSensorPpm);
+
+//   globalPpm = (9.0 * globalPpm + ppm) / 10;
+
+//   int milliVolts = analogReadMilliVolts(pin);
+
+//   // unsigned long percent = map(signal, 0, 4095, 0, 100);
+//   Serial.print(", milli-volts: ");
+//   Serial.print(milliVolts);
+//   Serial.print(", signal: ");
+//   Serial.print(signal);
+//   Serial.print(", ppm: ");
+//   Serial.print(ppm);
+//   Serial.print(", globalPpm: ");
+//   Serial.print(globalPpm);
+//   Serial.print(", percent: ");
+//   Serial.print(toPercent(signal));
+// }
 
 void loop() {
 
-  Serial.print(" MQ2 ");
-  getInfo(MQ5pin, 300, 10000);
+  float signal;
+  unsigned long ppm;
+  int coPpm;
+  int ch4Ppm;
+  int lpgPpm;
+  int h2Ppm;
+
+  signal = mux.read(0);
+
+  lpgPpm = map(signal, 0, 4095, 200, 5000);
+  int butPpm = map(signal, 0, 4095, 300, 5000);
+  ch4Ppm = map(signal, 0, 4095, 5000, 20000);
+  h2Ppm = map(signal, 0, 4095, 300, 5000);
+  int alPpm = map(signal, 0, 4095, 100, 2000);
+  Serial.print(" MQ2: " + String(signal) + " - ");
+  Serial.print(" " + String(lpgPpm) + "(LPG)");
+  Serial.print(" " + String(butPpm) + "(BUT)");
+  Serial.print(" " + String(ch4Ppm) + "(CH4)");
+  Serial.print(" " + String(h2Ppm) + "(H2)");
+  Serial.print(" " + String(alPpm) + "(AL)");
+
+  Serial.println("");
+
+  signal = mux.read(1);
+  float mapped = map(signal, 0, 4095, 5, 1000);
+  Serial.print(" MQ3: " + String(signal) + " - ");
+  Serial.print(" " + String(mapped / 100) + "(Alcohol gas)");
+
+  Serial.println("");
+
+  signal = mux.read(2);
+  ppm = map(signal, 0, 4095, 200, 10000);
+  Serial.print(" MQ4: " + String(signal) + " - ");
+  Serial.print(" " + String(ppm) + "(CH4)");
+
+  Serial.println("");
+
+  signal = mux.read(3);
+  ppm = map(signal, 0, 4095, 200, 10000);
+  Serial.print(" MQ5: " + String(signal) + " - ");
+  Serial.print(" " + String(ppm) + "(CH4/LPG)");
+
+  Serial.println("");
+
+  signal = mux.read(4);
+  ppm = map(signal, 0, 4095, 200, 10000);
+  Serial.print(" MQ6: " + String(signal) + " - ");
+  Serial.print(" " + String(ppm) + "(LPG)");
+
+  Serial.println("");
+
+  signal = mux.read(5);
+  ppm = map(signal, 0, 4095, 20, 2000);
+  Serial.print(" MQ7: " + String(signal) + " - ");
+  Serial.print(" " + String(ppm) + "(CO)");
+
+  Serial.println("");
+
+  signal = mux.read(6);
+  ppm = map(signal, 0, 4095, 100, 10000);
+  Serial.print(" MQ8: " + String(signal) + " - ");
+  Serial.print(" " + String(ppm) + "(H2)");
+
+  Serial.println("");
+
+  signal = mux.read(7);
+  coPpm = map(signal, 0, 4095, 20, 2000);
+  ch4Ppm = map(signal, 0, 4095, 500, 10000);
+  lpgPpm = map(signal, 0, 4095, 500, 10000);
+  Serial.print(" MQ9: " + String(signal) + " - ");
+  Serial.print(" " + String(coPpm) + "(CO)");
+  Serial.print(" " + String(ch4Ppm) + "(CH4)");
+  Serial.print(" " + String(lpgPpm) + "(LPG)");
+
+  Serial.println("");
+
+  // delay(100);
+
+  // 300-10000
+
+  // getInfo(SignalPin, 300, 10000);
 
   // Serial.print(" MQ3: ");
   // getInfo(MQ3pin);
@@ -116,6 +195,7 @@ void loop() {
   // getInfo(MQ9pin, 10, 10000);
 
   Serial.println("");
+  Serial.println("-------------------------------------------");
 
-  delay(1000); // wait 2s for next reading
+  delay(2000); // wait 2s for next reading
 }
